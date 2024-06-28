3cb97ff7ab92afcf6de6073161761c256e717893
AbdulRahman Ayman <140063652+AbdulRahmanAMKhalaf@users.noreply.github.com>
2024-06-29 02:57:23 +0300

    Update on_bourding_screen_content.dart

--- lib/screens/on_bourding_screen/on_bourding_screen_content.dart
+++ lib/screens/on_bourding_screen/on_bourding_screen_content.dart
@@ -12,7 +12,6 @@ class OnBourdingScreenContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final PageController pageController = PageController();
     int index=0;
-    AnimationController controller= ;
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
@@ -44,71 +43,68 @@ class OnBourdingScreenContent extends StatelessWidget {
           ),
           child: Padding(
             padding: EdgeInsets.all(5.w),
-            child: SlideTransition(
-              position: ,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(bourdingList[index].title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 30.px,
-                      fontWeight: FontWeight.w800,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(bourdingList[index].title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 30.px,
+                    fontWeight: FontWeight.w800,
 
-                    ),
                   ),
-                  SizedBox(height: 1.5.h,),
-                  Text(bourdingList[index].text,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.grey.shade400,
-                      fontSize: 15.px,
-                      fontWeight: FontWeight.w800,
-                    ),
+                ),
+                SizedBox(height: 1.5.h,),
+                Text(bourdingList[index].text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontSize: 15.px,
+                    fontWeight: FontWeight.w800,
                   ),
-                  SizedBox(height: 2.5.h,),
-                  SmoothPageIndicator(
-                    controller: pageController,
-                    count: bourdingList.length,
-                    effect: ExpandingDotsEffect(
-                        activeDotColor: HexColor('#df6f47'),
-                        dotColor: Colors.grey.shade300,
-                        dotHeight: 1.h,
-                        dotWidth: 2.w,
-                        expansionFactor: 0.7.w,
-                        spacing: 00.5.w),
-                  ),
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.px),
-                      child: MaterialButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                            MaterialPageRoute(
-                              builder:(context)=>const LoginScreenView(),
-                            ),
-                          );
-                        },
-                        height:7.h,
-                        color: HexColor('#df6f47'),
-                        child: Center(
-                          child: Text(
-                            'Cotinue',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 30.px,
-                                color: Colors.grey.shade200),
+                ),
+                SizedBox(height: 2.5.h,),
+                SmoothPageIndicator(
+                  controller: pageController,
+                  count: bourdingList.length,
+                  effect: ExpandingDotsEffect(
+                      activeDotColor: HexColor('#df6f47'),
+                      dotColor: Colors.grey.shade300,
+                      dotHeight: 1.h,
+                      dotWidth: 2.w,
+                      expansionFactor: 0.7.w,
+                      spacing: 00.5.w),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.px),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                          MaterialPageRoute(
+                            builder:(context)=>const LoginScreenView(),
                           ),
+                        );
+                      },
+                      height:7.h,
+                      color: HexColor('#df6f47'),
+                      child: Center(
+                        child: Text(
+                          'Cotinue',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 30.px,
+                              color: Colors.grey.shade200),
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
